c53e01a6f334f58e07c43fbc1d5b8652c5a28bd8
MySmallfish <mysmallfish@gmail.com>
2013-09-08 07:53:55 +0300

    Login

--- app/js/services/login-manager.js
+++ app/js/services/login-manager.js
@@ -1,21 +1,29 @@
-(function(S){
-	S.LoginManager = function(storageService, $q){
-		var userLoggedIn = false;
-		function isLoggedIn(){
-			return userLoggedIn;
-		}
-		
-		function login(userName, password){
-		    userLoggedIn = true;
-		    var result = $q.defer();
-		    var token = "ABC123";
-		    result.resolve(token);
-		    return result.promise;
-		}
-		
-		return {
-			isUserLoggedIn: isLoggedIn,
-			login: login
-		}
-	}
+(function(S) {
+    S.LoginManager = function(storageService, $q) {
+        var userLoggedIn = false;
+
+        function isLoggedIn() {
+            return userLoggedIn;
+        }
+
+        function login(userName, password) {
+            
+            storageService.prefix("Inspector").session("userName", userName).then(function () {
+                return storageService.session("userName").then(function (v) {
+                    console.log("MYVALUE", v);
+                    return this;
+                });
+            });
+            userLoggedIn = true;
+            var result = $q.defer();
+            var token = "ABC123";
+            result.resolve(token);
+            return result.promise;
+        }
+
+        return {
+            isUserLoggedIn: isLoggedIn,
+            login: login
+        };
+    };
 })(Simple);
--- app/js/services/storage-service.js
+++ app/js/services/storage-service.js
@@ -1,3 +1,57 @@
-(function(S){
-	S.StorageService = function(){}
+(function(S) {
+    S.StorageService = function ($q) {
+        var prefix = "";
+        function deserialize(value) {
+            var result = $q.defer();
+            result.resolve(value ? JSON.parse(value) : null);
+            return result.promise;
+        }
+        function serialize(value) {
+            var result = $q.defer();
+            result.resolve(value ? JSON.stringify(value) : "");
+            return result.promise;
+        }
+        var wrappers = {
+            session: {
+                get: function(key) {
+                    return deserialize(sessionStorage.getItem(key));
+                },
+                set: function(key, value) {
+                    return serialize(value).then(function(serializedValue) {
+                        sessionStorage.setItem(key, serializedValue);
+                        return serializedValue;
+                    });
+                }
+            }    
+        };
+
+        function item(type, key, value) {
+            
+            var wrapper = wrappers[type];
+            if (wrapper) {
+                key = (prefix || "") + ":" + key;
+                if (typeof value !== "undefined") {
+                    return wrapper.set(key, value);
+                } else {
+                    return wrapper.get(key);
+                }
+            } else {
+                throw new Error("Storage Type '" + type + "' is not configured.");
+            }
+        }
+        
+        return {
+            session: function(key, value) {
+                return item("session", key, value);
+            },
+            prefix: function(newPrefix) {
+                if (typeof newPrefix !== "undefined") {
+                    prefix = newPrefix;
+                    return this;
+                } else {
+                    return prefix;
+                }
+            }
+        }
+    };
 })(Simple);

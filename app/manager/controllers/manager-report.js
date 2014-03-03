1748f2e441b0c99a75a84258967e3ae2a57d8186
Yair Cohen <mysmallfish@gmail.com>
2014-03-03 13:40:34 +0200

    manager report

--- app/manager/controllers/manager-report.js
+++ app/manager/controllers/manager-report.js
@@ -1,8 +1,22 @@
 (function (S, I) {
-    I.ManagerReportController = ["$scope", function ($scope) {
+    I.ManagerReportController = ["$scope", "timeReportManager", function ($scope, timeReportManager) {
         $scope.changeHeader({
             header: "ManagerReport",
             back: true
+        });
+
+        $scope.managerScanEnter = function () {
+            $scope.status = "enter";
+            console.log("scanEnter");
+        };
+        
+        $scope.managerScanExit = function () {
+            $scope.status = "exit";
+            console.log("scanExit");
+        };
+
+        timeReportManager.getEvents().then(function (items) {
+            $scope.events = items;
         });
     }];
 })(Simple, Simple.Inspector);

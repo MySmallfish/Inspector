5fab56f1bb5355e55b94695472f6bcbb468d6ee0
MySmallfish <mysmallfish@gmail.com>
2014-03-06 17:32:06 +0200

    1.0.2

--- app/manager/controllers/event-time-reports.js
+++ app/manager/controllers/event-time-reports.js
@@ -1,6 +1,11 @@
 ﻿(function (S, I) {
     I.EventTimeReportsController = ["$scope", "eventReportManager", "employeeService", function ($scope, eventReportManager, employeeService) {
 
+        $scope.changeHeader({
+            header: "EventTimeReports",
+            back: true
+        });
+
         eventReportManager.getEventById(11).then(function (item) {
             $scope.event = item;
         });
